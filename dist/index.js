2c8e5479235c036b2d02505dbcba811c936c580b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-02-25 12:42:35 -0300

    build(dist): automatic new publish

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+module.exports = {
+    "extends": ["stylelint-config-standard"],
+    "plugins": [
+        "stylelint-order"
+    ],
+    "rules": {
+        "order/order": [
+            "custom-properties",
+            "declarations"
+        ],
+        "order/properties-alphabetical-order": true
+    }
+};
